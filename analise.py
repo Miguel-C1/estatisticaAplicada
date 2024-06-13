464ec4230630cf99e9cc6722f4d4ece15c4b511e
Miguel Carvalho <doxito007@gmail.com>
2024-06-13 18:33:38 -0300

    Adicionando Arquivos tratados do .csv e também as analises descritivas

--- analise.py
+++ analise.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy.stats import norm, shapiro, normaltest, kstest
+from scipy.stats import norm, shapiro, normaltest, kstest, f_oneway
 
 # Função para limpar e converter os dados
 def clean_data(value):
@@ -16,75 +16,122 @@ def clean_data(value):
         return value
 
 # Carregar os dados do arquivo CSV corrigido
-data = pd.read_csv('SAO_PAULO_TAUBATE_clean.csv', delimiter=';', decimal=',')
+data = pd.read_csv('BAHIA_BELMONTE_clean.csv', delimiter=';', decimal=',')
 
-def dist_norml(data: pd.DataFrame):
-    # Visualização de Dados
-    for column in data.columns[2:]:
+def dist_norml(data: pd.DataFrame, columns=None):
+    if columns is None:
+        columns = data.columns[2:]  # Seleciona todas as colunas numéricas após as duas primeiras
+
+    for column in columns:
         sns.histplot(data[column], stat="density", linewidth=0)
+        mean = data[column].mean()
+        std_dev = data[column].std()
         xmin, xmax = plt.xlim()
         x = np.linspace(xmin, xmax, 100)
-        p = norm.pdf(x, mean[column], std_dev[column])
+        p = norm.pdf(x, mean, std_dev)
         plt.plot(x, p, 'k', linewidth=2)
-        title = "Distribuição Normal: {}".format(column)
+        title = f"Distribuição Normal: {column}"
         plt.title(title)
         plt.xlabel(column)
         plt.ylabel('Densidade')
         plt.show()
 
-def dist_norml_prd(data_standardized: pd.DataFrame):
-    # Análise de Probabilidades
-    for column in data_standardized.columns:
+def dist_norml_prd(data_standardized: pd.DataFrame, columns=None):
+    if columns is None:
+        columns = data_standardized.columns  # Seleciona todas as colunas numéricas
+
+    for column in columns:
         sns.histplot(data_standardized[column], kde=True, stat="density", linewidth=0)
         xmin, xmax = plt.xlim()
         x = np.linspace(xmin, xmax, 100)
-        p = norm.pdf(x, 0, 1)
+        p = norm.pdf(x, 0, 1)  # Média 0 e desvio padrão 1 para distribuição normal padrão
         plt.plot(x, p, 'k', linewidth=2)
-        title = "Distribuição Normal Padronizada: {}".format(column)
+        title = f"Distribuição Normal Padronizada: {column}"
         plt.title(title)
         plt.xlabel(column)
         plt.ylabel('Densidade')
         plt.show()
+
+# Função para visualizar distribuição normal por mês
+def dist_norml_by_month(data: pd.DataFrame, columns=None):
+    
+    if columns is None:
+        columns = data.columns[2:]
+        
+    # Converter a coluna de data para datetime
+    data['Data'] = pd.to_datetime(data['Data'], format='%d/%m/%Y')
+    
+    # Extrair o mês da data
+    data['Mês'] = data['Data'].dt.month_name()
+
+    # Loop pelos meses únicos
+    unique_months = data['Mês'].unique()
+    for month in unique_months:
+        month_data = data[data['Mês'] == month]
+
+        # Selecionar colunas numéricas após as duas primeiras
+
+        for column in columns:
+            sns.histplot(month_data[column], stat="density", linewidth=0)
+            mean = month_data[column].mean()
+            std_dev = month_data[column].std()
+            xmin, xmax = plt.xlim()
+            x = np.linspace(xmin, xmax, 100)
+            p = norm.pdf(x, mean, std_dev)
+            plt.plot(x, p, 'k', linewidth=2)
+            title = f"Distribuição Normal - {month}: {column}"
+            plt.title(title)
+            plt.xlabel(column)
+            plt.ylabel('Densidade')
+            plt.show()
 
 # Aplicar a função de limpeza em todas as colunas numéricas
 for column in data.columns[2:]:  # Ignora as duas primeiras colunas que são Data e Hora
     data[column] = data[column].apply(clean_data)
 
-
 numeric_data = data.iloc[:, 2:]  # Seleciona apenas as colunas numéricas após as duas primeiras
 
-# Análise Descritiva
-mean = numeric_data.mean()
-median = numeric_data.median()
-mode = numeric_data.mode().iloc[0]
-std_dev = numeric_data.std()
-variance = numeric_data.var()
-kurtosis = numeric_data.kurtosis()
-skewness = numeric_data.skew()
+def analise_desc(numeric_data: pd.DataFrame):
+    # Análise Descritiva
+    mean = numeric_data.mean()
+    median = numeric_data.median()
+    mode = numeric_data.mode().iloc[0]
+    std_dev = numeric_data.std()
+    variance = numeric_data.var()
+    kurtosis = numeric_data.kurtosis()
+    skewness = numeric_data.skew()
 
-print(f"Média: \n{mean}\n")
-print(f"Mediana: \n{median}\n")
-print(f"Moda: \n{mode}\n")
-print(f"Desvio Padrão: \n{std_dev}\n")
-print(f"Variância: \n{variance}\n")
-print(f"Curtose: \n{kurtosis}\n")
-print(f"Assimetria: \n{skewness}\n")
-
-# Criar DataFrame com estatísticas descritivas
-desc_stats = pd.DataFrame({
-    'Média': mean,
-    'Mediana': median,
-    'Moda': mode,
-    'Desvio Padrão': std_dev,
-    'Variância': variance,
-    'Curtose': kurtosis,
-    'Assimetria': skewness
-})
-
-# Salvar DataFrame em um arquivo CSV
-desc_stats.to_csv('analise_descritiva.csv', index=True)
+    quartiles = numeric_data.quantile([0.25, 0.5, 0.75])
 
 
+    print(f"Média: \n{mean}\n")
+    print(f"Mediana: \n{median}\n")
+    print(f"Moda: \n{mode}\n")
+    print(f"Desvio Padrão: \n{std_dev}\n")
+    print(f"Variância: \n{variance}\n")
+    print(f"Curtose: \n{kurtosis}\n")
+    print(f"Assimetria: \n{skewness}\n")
+    print(f"Quartis: \n{quartiles}\n")
+
+    # Criar DataFrame com estatísticas descritivas
+    desc_stats = pd.DataFrame({
+        'Média': mean,
+        'Mediana': median,
+        'Moda': mode,
+        'Desvio Padrão': std_dev,
+        'Variância': variance,
+        'Curtose': kurtosis,
+        'Assimetria': skewness,
+        '1º Quartil': quartiles.loc[0.25],
+        '2º Quartil (Mediana)': quartiles.loc[0.5],
+        '3º Quartil': quartiles.loc[0.75]
+    })
+    # Salvar DataFrame em um arquivo CSV
+    desc_stats.to_csv('analise_descritiva.csv', index=True)
+    
+    return mean, std_dev
+
+mean, std_dev = analise_desc(numeric_data)
 
 # Testes de Normalidade
 def normality_tests(column_data):
@@ -106,4 +153,14 @@ data_standardized = (data[data.columns[2:]] - mean) / std_dev
 print(data_standardized.head())
 
 
+
+#dist_norml(data=data, columns=['Temp. Ins. (C)', 'Temp. Max. (C)', 'Temp. Min. (C)'])
+
+
+# ['Data', 'Hora (UTC)', 'Temp. Ins. (C)', 'Temp. Max. (C)',
+#       'Temp. Min. (C)', 'Umi. Ins. (%)', 'Umi. Max. (%)', 'Umi. Min. (%)',   
+#       'Pto Orvalho Ins. (C)', 'Pto Orvalho Max. (C)', 'Pto Orvalho Min. (C)',
+#      'Pressao Ins. (hPa)', 'Pressao Max. (hPa)', 'Pressao Min. (hPa)',      
+#      'Vel. Vento (m/s)', 'Dir. Vento (m/s)', 'Raj. Vento (m/s)',
+#      'Radiacao (KJ/m²)', 'Chuva (mm)']
 
